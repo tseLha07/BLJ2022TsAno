82f3fe3eb3e70dd8f79015b718f50131fd1ed47f
kaama07 <tsering.anodunkhartsang@edu.tbz.ch>
2023-02-13 15:37:44 +0100

    did the task 02 from java

---
+++ Java/KW_2023_07/02_variablen-datentypen-operatoren/Stoffwechselrate/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,9 @@
+package ch.noseryoung.blj;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            calculator.getUserInput();
+        }
+}
---
+++ Java/KW_2023_07/02_variablen-datentypen-operatoren/Stoffwechselrate/src/ch/noseryoung/blj/calculator.java
@@ -0,0 +1,50 @@
+package ch.noseryoung.blj;
+import java.util.Scanner;
+
+public class calculator {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void getUserInput(){
+        System.out.println("Enter your biological sex:");
+        String sex = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter your weight in kg: ");
+        float weight = Float.parseFloat(scanner.nextLine());
+        System.out.println("Enter your height in cm: ");
+        float height = Float.parseFloat(scanner.nextLine());
+
+        if(sex.equals("m")){
+
+            float m1 = (float) (13.7 * weight);
+            float m2 = (float) (5 * height);
+            float m3 = (float) (8 * age);
+            float result = (float) (66.47 + m1 + m2 + m3 - 6);
+
+            System.out.println("Calorie rate");
+            System.out.println("-----------------------------");
+            System.out.println("Sex:                " + sex);
+            System.out.println("age:                " + age);
+            System.out.println("Weight (in kg):     " + weight);
+            System.out.println("Height (in cm):     " + height);
+            System.out.println("-----------------------------");
+            System.out.println("Calorie per day: " + result);
+        } else if (sex.equals("f")) {
+
+            float m1 = (float) (9.6 * weight);
+            float m2 = (float) (1.8 * height);
+            float m3 = (float) (-4.7 * age);
+            float result = (float) (655.1 + m1 + m2 + m3);
+
+            System.out.println("Calorie rate");
+            System.out.println("-----------------------------");
+            System.out.println("Sex:                " + sex);
+            System.out.println("age:                " + age);
+            System.out.println("Weight (in kg):     " + weight);
+            System.out.println("Height (in cm):     " + height);
+            System.out.println("-----------------------------");
+            System.out.println("Calorie per day: " + result);
+        }
+    }
+}
