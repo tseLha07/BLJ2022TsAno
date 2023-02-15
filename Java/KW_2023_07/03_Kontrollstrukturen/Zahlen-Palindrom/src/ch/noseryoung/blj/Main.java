c9917a62cf54f8d7bb3bbeaf4519512a184677ab
kaama07 <tsering.anodunkhartsang@edu.tbz.ch>
2023-02-15 08:54:54 +0100

    completed Task07

---
+++ Java/KW_2023_07/03_Kontrollstrukturen/Zahlen-Palindrom/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,7 @@
+package ch.noseryoung.blj;
+
+public class Main {
+    public static void main(String[] args) {
+        method.checksTheNumber();
+    }
+}
---
+++ Java/KW_2023_07/03_Kontrollstrukturen/Zahlen-Palindrom/src/ch/noseryoung/blj/method.java
@@ -0,0 +1,56 @@
+package ch.noseryoung.blj;
+import java.util.Scanner;
+
+public class method {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void checksTheNumber(){
+
+        for (int i = 1; i > 0;){
+            System.out.println("----------------------------");
+            System.out.print("Enter any number: ");
+            int num = Integer.parseInt(scanner.nextLine());
+
+            int reverse_number = reverseNumber(num);
+            System.out.println("reversed number: " + reverse_number);
+
+            System.out.println("----------------------------");
+
+            if(num == reverse_number){
+                System.out.println(reverse_number + " is a Palindrome.");
+                System.out.println("----------------------------");
+            } else{
+                System.out.println(reverse_number + " is not a Palindrome.");
+                System.out.println("----------------------------");
+            }
+
+            System.out.println("continue (c)");
+            System.out.println("quit (q)");
+            String o = scanner.nextLine();
+
+            if(o.equals("q")){
+                break;
+            }
+        }
+    }
+    public static int reverseNumber(int number)
+    {
+        boolean isNoNegative = number < 0 ? true : false;
+        if(isNoNegative)
+        {
+            number = number * -1;
+        }
+        int reverse = 0;
+        int lastDigit = 0;
+        while (number >= 1)
+        {
+            lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
+            number = number / 10;
+        }
+
+        return isNoNegative == true? reverse*-1 : reverse;
+    }
+
+}
